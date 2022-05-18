a86f33e85ff7549c702bb7c57f42a43729930cf9
unknown <interkp12@gmail.com>
2022-05-18 16:52:38 +0900

    training

---
+++ 220518-js-training.js
@@ -0,0 +1,90 @@
+
+
+// console.log('test');
+
+// const memberOne = {
+
+//   name = '임성민',
+//   gender = male,
+//   age = 36,
+//   deskNumber = 1,
+//   glasses = true,
+//   married = 'fuck',
+//   watch = false,
+//   phone = 's21'
+
+// }
+
+// const memberTwo = {
+
+//   name = '누굴까',
+//   gender = female,
+//   age = 00,
+//   deskNumber = 2,
+//   glasses = true,
+//   married = 'yes',
+//   watch = false,
+//   phone = 'apple'
+
+// }
+
+// console.log(memberOne.name);
+// console.log(memberTwo.name);
+
+//* 생성자 함수 : constructor function
+//* 객체를 찍어내는 거푸집 틀
+//* 석고 뜨는 함수 
+
+// function Member(name, gender, age, deskNumber, gender, married, watch, phone) {
+
+//   this.name =  name,
+//   this.gender =  gender,
+//   this.age = age,
+//   this.deskNumber = deskNumber,
+//   this.glasses = gender,
+//   this.married = married,
+//   this.watch = watch,
+//   this.phone = phone;
+// }
+
+// const memberOne = new Member("임성민", "male", "36", "1", "yes", "fuck", "yes", "s21");
+// console.dir(memberOne);
+// 111
+
+// const testOne = new Date();
+// console.log(testOne.getHours());
+
+// function ampm(hour, msg) {
+// if(hour > 12) {
+//   console.log(msg + (hour-12) + "시 입니다");
+//   console.log(memberOne.phone); 
+
+// } else{
+
+//   console.log("오전입니다 : " + hour);
+
+//   }
+  
+// }
+
+// ampm(testOne.getHours(), "오후입니다");
+
+
+const day = new Date("1987-07-28");
+
+console.log(day);
+
+function diffDay() {
+
+  const namenTime = new Date("2022-07-28");
+  const todayTime = new Date();
+
+  const diff = namenTime - todayTime;
+
+  const diffDay 
+
+}
+
+
+
+
